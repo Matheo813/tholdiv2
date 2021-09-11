5a12f03d686299264e48f8054df9f1632d6965b9
Saami783 <72657906+Saami783@users.noreply.github.com>
2021-09-11 20:36:33 +0200

    Tholdi 11/09/2021
    Projet tholdi mis à jours le 11/09/2021
    - BDD Fonctionnel
    - Erreur syntax check = $bdd qui bloque l'inscription / connexion.
    - Formulaire de reservation fait pour la date ainsi que le nombre de conteneurs souhaités.

---
+++ test/connexion.php
@@ -0,0 +1,125 @@
+<?php 
+    session_start();
+    require_once '_gestionBase_inc.php';
+    include_once "_head_inc.php";
+
+    if(!empty($_POST['login']) && !empty($_POST['mdp']))
+    {
+        $email = htmlspecialchars($_POST['login']);
+        $mdp = htmlspecialchars($_POST['mdp']);
+
+
+
+        $check = $bdd ->prepare('SELECT mdp, login FROM utilisateur');
+        $check->execute(array($email));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        if($row == 1)
+        {
+            if(filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                
+                if(password_verify($mdp, $data['mdp']))
+                {
+                    $_SESSION['login'] = $data['login'];
+                    header('Location: landing.php');
+                    die();
+                }else{ header('Location: index.php?login_err=password'); die(); }
+            }else{ header('Location: index.php?login_err=email'); die(); }
+        }else{ header('Location: index.php?login_err=already'); die(); }
+    }
+?>
+<!DOCTYPE html>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="author" content="NoS1gnal"/>
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <title>Connexion</title>
+        </head>
+        <body>
+        
+        <div class="login-form">
+             <?php 
+                if(isset($_GET['login_err']))
+                {
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'mdp':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'login':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte non existant
+                            </div>
+                        <?php
+                        break;
+                    }
+                }
+                ?> 
+            
+            <form action="connexion.php" method="post">
+                <h2 class="text-center">Connexion</h2>       
+                <div class="form-group">
+                    <input type="login" name="login" class="form-control" placeholder="login" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="mdp" name="mdp" class="form-control" placeholder="mdp" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Connexion</button>
+                </div>   
+            </form>
+            <p class="text-center"><a href="inscription.php">Inscription</a></p>
+        </div>
+        <style>
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+
+            body
+            {
+                background-color: #4169E1;
+            }
+
+        </style>
+        </body>
+</html>
